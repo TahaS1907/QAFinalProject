f3ae1b4cc5f6705826b53019903559ac4dfb10db
Taha Sari <saritaha1907@gmail.comgit config --global user.email saritaha1907@gmail.comgit config --global user.email saritaha1907@gmail.com>
2022-11-25 17:34:13 +0300

    Commit-2 bugs fixed

--- src/test/java/Tests/TestOtomation.java
+++ src/test/java/Tests/TestOtomation.java
@@ -38,22 +38,33 @@ public class TestOtomation {
         Thread.sleep(3000);
         WebElement searchButton = driver.findElement(By.tagName("input"));
         searchButton.sendKeys("Kaşık maması");
-        Thread.sleep(2000);
+        Thread.sleep(10000);
+        searchButton.sendKeys(Keys.RETURN);
+        Thread.sleep(10000);
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        //js.executeScript("scroll(0, 250);");
-        //Thread.sleep(2000);
-        driver.findElement(By.xpath("//*[@id=\"searchContent\"]/div[1]/div[1]/div[1]/ul[1]/li[3]/a[1]/div[1]/div[1]/h3[1]")).click();
-        Thread.sleep(5000);
-        driver.findElement(By.id("addToCartBtn")).click();
-        Thread.sleep(4000);
+        js.executeScript("scroll(0, 1250);");
+        Thread.sleep(10000);
+        js.executeScript("scroll(1250, 2500);");
+        Thread.sleep(10000);
+        js.executeScript("scroll(2500, 3750);");
+        Thread.sleep(10000);
+        js.executeScript("scroll(3750, 5000);");
+        Thread.sleep(10000);
+        js.executeScript("scroll(5000, 5500);");
+        Thread.sleep(10000);
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.findElements(By.id("addToCartBtn")).get(42).click();
+        Thread.sleep(10000);
         driver.findElement(By.id("btnShowCart")).click();
+        String control= driver.getTitle();
+        Assert.assertEquals(control,"Ara Kaşık maması - ebebek");
         Thread.sleep(8000);
         driver.findElement(By.id("btnGoToShippingAddress")).click();
+        Thread.sleep(20000);
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.findElement(By.id("btnSubmit")).click();
-        driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
-
-        driver.quit();//btnSubmitLoginüyegirişikontrol
+        String end= driver.getTitle();
+        Assert.assertEquals(end,"Giriş Yap");
+        Thread.sleep(5000);
+        driver.quit();
 
     }}
